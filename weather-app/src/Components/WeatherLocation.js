d23656372c3878669d4bd1d7272e0f05a40ae59e
fedeSanchez <fedeericosanchez18@gmail.com>
2019-02-12 16:27:38 -0300

    creo componentes Location y weatherData

---
+++ weather-app/src/Components/WeatherLocation.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Location from './Location'
+import WeatherData from './WeatherData'
+
+const WeatherLocaltion = () => (
+    <div>
+       
+        <Location></Location>
+        <WeatherData></WeatherData>
+    </div>
+    );
+
+export default WeatherLocaltion;
