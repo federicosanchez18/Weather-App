0f99929ca0e75f6ec42d35b09006f2eac456c345
fedeSanchez <fedeericosanchez18@gmail.com>
2019-02-22 10:11:18 -0300

    creo eventos

--- weather-app/src/Components/WeatherLocation/index.js
+++ weather-app/src/Components/WeatherLocation/index.js
@@ -3,6 +3,7 @@ import Location from './Location'
 import WeatherData from './WeatherData'
 import {
     SUN,
+    WINDY,
 } from './../../Constant/weathers';
 import './styles.css' 
 
@@ -12,12 +13,37 @@ const data = {
     humidity : 10 ,
     wind : '10 m/s '
 }
+
+const data2 = {
+    temperature: 15,
+    weatherState: WINDY,
+    humidity: 30,
+    wind: '10 m/s '
+}
 class WeatherLocaltion  extends Component {
+    
+    constructor(){
+        super();
+        this.state = {
+            city : "Brazil",
+            data : data,
+        }
+    }
+
+    handleUpdateClick = () => {
+        console.log("actualizado");
+        this.setState  ({
+            city : 'Buenos Aires',
+            data : data2,
+        });
+    }
     render(){
+        const {city, data} = this.state;
         return (
             <div className='weatherLocationCont'>
-            <Location city={'Wilde - Buenos Aires'}></Location>
+            <Location city={city}></Location>
             <WeatherData data={data}></WeatherData>
+            <button onClick = {this.handleUpdateClick}  >Actulizar</button>
             </div>
         );
     };
